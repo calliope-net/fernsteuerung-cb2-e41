741f8f9d8907e5199f650181de30d655da522afc
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-07-03 22:11:24 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    r_callibot.resetEncoder(r_callibot.eMotor.beide)
+    receiver.c2Motor255(150)
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    basic.showNumber(l)
+})
 radio.onReceivedData(function (receivedData) {
     if (radio.isBetriebsart(receivedData, radio.e0Betriebsart.p0)) {
         receiver.c2Motor255(radio.getByte(receivedData, radio.eBufferPointer.m0, radio.eBufferOffset.b0_Motor))
@@ -6,6 +13,8 @@ radio.onReceivedData(function (receivedData) {
         radio.zeige5x5Joystick(receivedData)
     }
 })
+let list: number[] = []
+let l = 0
 receiver.beimStart(
 receiver.eModell.calli2bot,
 90,
@@ -13,3 +22,10 @@ true,
 65,
 175
 )
+loops.everyInterval(200, function () {
+    list = r_callibot.encoderValue()
+    l = list[0]
+    if (l > 50 * 31.25) {
+        receiver.c2Motor255(128)
+    }
+})
