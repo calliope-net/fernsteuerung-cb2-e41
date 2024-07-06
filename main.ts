bebc4a8630598eb9d365e158902f2060de760602
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-07-06 19:57:27 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,26 +4,19 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
 })
 radio.onReceivedData(function (receivedData) {
     if (radio.isBetriebsart(receivedData, radio.e0Betriebsart.p0)) {
-        receiver.c2motor128lenken16(radio.getByte(receivedData, radio.eBufferPointer.m0, radio.eBufferOffset.b0_Motor), radio.getByte(receivedData, radio.eBufferPointer.m0, radio.eBufferOffset.b1_Servo))
-        receiver.rgbLEDs(receiver.eRGBled.a, 0x0000ff, true)
-        radio.zeige5x5Buffer(receivedData)
-        radio.zeige5x5Joystick(receivedData)
+        if (radio.getByte(receivedData, radio.eBufferPointer.m0, radio.eBufferOffset.b0_Motor) > 128 && receiver.c2UltraschallEntfernung(receiver.eDist.cm) < 25) {
+            receiver.c2motor128lenken16(128, 16)
+        } else {
+            receiver.c2motor128lenken16(radio.getByte(receivedData, radio.eBufferPointer.m0, radio.eBufferOffset.b0_Motor), radio.getByte(receivedData, radio.eBufferPointer.m0, radio.eBufferOffset.b1_Servo))
+        }
     }
+    receiver.rgbLEDs(receiver.eRGBled.a, 0x0000ff, true)
+    radio.zeige5x5Buffer(receivedData)
+    radio.zeige5x5Joystick(receivedData)
 })
-let l = 0
-let list: number[] = []
 receiver.beimStart(
 receiver.eHardware.calli2bot,
 90,
 true,
 65
 )
-loops.everyInterval(200, function () {
-    if (radio.isBetriebsart(radio.radio_receivedBuffer19(), radio.e0Betriebsart.p2)) {
-        list = r_callibot.encoderValue()
-        l = list[0]
-        if (l > 50 * 31.25) {
-        	
-        }
-    }
-})
