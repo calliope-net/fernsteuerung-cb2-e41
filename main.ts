cb42c95b459e37b352c2598eeb3efe7e854e82c0
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-07-06 11:20:24 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,7 +10,7 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
 })
 radio.onReceivedData(function (receivedData) {
     if (radio.isBetriebsart(receivedData, radio.e0Betriebsart.p0)) {
-        receiver.c2Motor255(radio.getByte(receivedData, radio.eBufferPointer.m0, radio.eBufferOffset.b0_Motor))
+        receiver.c2motor128(radio.getByte(receivedData, radio.eBufferPointer.m0, radio.eBufferOffset.b0_Motor), radio.getByte(receivedData, radio.eBufferPointer.m0, radio.eBufferOffset.b1_Servo), 40)
         receiver.rgbLEDs(receiver.eRGBled.a, 0x0000ff, true)
         radio.zeige5x5Buffer(receivedData)
         radio.zeige5x5Joystick(receivedData)
