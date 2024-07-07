11d33cc3c129a1ab527a6117a071e9b362284f52
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-07-07 20:59:58 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,10 +4,10 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
 })
 radio.onReceivedData(function (receivedData) {
     if (radio.isBetriebsart(receivedData, radio.e0Betriebsart.p0)) {
-        if (radio.getByte(receivedData, radio.eBufferPointer.m0, radio.eBufferOffset.b0_Motor) > 128 && receiver.c2UltraschallEntfernung(receiver.eDist.cm) < 25) {
-            receiver.c2motor128lenken16(128, 16)
+        if (radio.getByte(receivedData, radio.eBufferPointer.m0, radio.eBufferOffset.b0_Motor) > 128 && cb2.readUltraschallEntfernung(cb2.eDist.cm) < 25) {
+            cb2.writeMotor128Servo16(128, 16)
         } else {
-            receiver.c2motor128lenken16(radio.getByte(receivedData, radio.eBufferPointer.m0, radio.eBufferOffset.b0_Motor), radio.getByte(receivedData, radio.eBufferPointer.m0, radio.eBufferOffset.b1_Servo))
+            cb2.writeMotor128Servo16(radio.getByte(receivedData, radio.eBufferPointer.m0, radio.eBufferOffset.b0_Motor), radio.getByte(receivedData, radio.eBufferPointer.m0, radio.eBufferOffset.b1_Servo))
         }
     }
     receiver.rgbLEDs(receiver.eRGBled.a, 0x0000ff, true)
