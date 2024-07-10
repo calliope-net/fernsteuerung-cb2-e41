14e3e3d797659742434bcc96f00d69febbd9cfb4
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-07-10 20:52:27 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,11 +10,19 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
 })
 radio.onReceivedData(function (receivedData) {
     if (radio.isBetriebsart(receivedData, radio.e0Betriebsart.p0) && radio.getaktiviert(receivedData, radio.e3aktiviert.m0)) {
-        if (radio.getByte(receivedData, radio.eBufferPointer.m0, radio.eBufferOffset.b0_Motor) > 128 && cb2.readUltraschallAbstand() < 25) {
-            cb2.writeMotor128Servo16(128, 16, 45)
+        if (radio.getSensor(receivedData, radio.eBufferPointer.m0, radio.eSensor.b6) && (radio.getByte(receivedData, radio.eBufferPointer.m0, radio.eBufferOffset.b0_Motor) > 128 && cb2.readUltraschallAbstand() < radio.getAbstand(receivedData))) {
+            cb2.writeMotor128Servo16(128, 16)
         } else {
-            cb2.writeMotor128Servo16(radio.getByte(receivedData, radio.eBufferPointer.m0, radio.eBufferOffset.b0_Motor), radio.getByte(receivedData, radio.eBufferPointer.m0, radio.eBufferOffset.b1_Servo))
+            cb2.writeMotor128Servo16(radio.getByte(receivedData, radio.eBufferPointer.m0, radio.eBufferOffset.b0_Motor), radio.getByte(receivedData, radio.eBufferPointer.m0, radio.eBufferOffset.b1_Servo), 45)
         }
+    } else if (radio.isBetriebsart(receivedData, radio.e0Betriebsart.p0) && radio.getaktiviert(receivedData, radio.e3aktiviert.mc)) {
+        if (radio.getByte(receivedData, radio.eBufferPointer.md, radio.eBufferOffset.b1_Servo) == 1) {
+            while (cb2.seite9Linienfolger(radio.getByte(receivedData, radio.eBufferPointer.mc, radio.eBufferOffset.b0_Motor), radio.getByte(receivedData, radio.eBufferPointer.mc, radio.eBufferOffset.b1_Servo), radio.getAbstand(receivedData))) {
+            	
+            }
+        }
+    } else {
+    	
     }
     receiver.rgbLEDs(receiver.eRGBled.a, 0x0000ff, true)
     radio.zeige5x5Buffer(receivedData)
