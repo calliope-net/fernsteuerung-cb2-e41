d0d055e56f78d11fb6e38db00ea89d913fcd01fc
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-07-11 18:19:35 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,21 +8,22 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     	
     }
 })
+function fahrenJoystick () {
+    if (radio.getSensor(radio.radio_receivedBuffer19(), radio.eBufferPointer.m0, radio.eSensor.b6) && (radio.getByte(radio.radio_receivedBuffer19(), radio.eBufferPointer.m0, radio.eBufferOffset.b0_Motor) > 128 && cb2.readUltraschallAbstand() < radio.getAbstand(radio.radio_receivedBuffer19()))) {
+        cb2.writeMotor128Servo16(128, 16)
+    } else {
+        cb2.writeMotor128Servo16(radio.getByte(radio.radio_receivedBuffer19(), radio.eBufferPointer.m0, radio.eBufferOffset.b0_Motor), radio.getByte(radio.radio_receivedBuffer19(), radio.eBufferPointer.m0, radio.eBufferOffset.b1_Servo), 45)
+    }
+}
 radio.onReceivedData(function (receivedData) {
     if (radio.isBetriebsart(receivedData, radio.e0Betriebsart.p0) && radio.getaktiviert(receivedData, radio.e3aktiviert.m0)) {
-        if (radio.getSensor(receivedData, radio.eBufferPointer.m0, radio.eSensor.b6) && (radio.getByte(receivedData, radio.eBufferPointer.m0, radio.eBufferOffset.b0_Motor) > 128 && cb2.readUltraschallAbstand() < radio.getAbstand(receivedData))) {
-            cb2.writeMotor128Servo16(128, 16)
-        } else {
-            cb2.writeMotor128Servo16(radio.getByte(receivedData, radio.eBufferPointer.m0, radio.eBufferOffset.b0_Motor), radio.getByte(receivedData, radio.eBufferPointer.m0, radio.eBufferOffset.b1_Servo), 45)
-        }
+        fahrenJoystick()
     } else if (radio.isBetriebsart(receivedData, radio.e0Betriebsart.p0) && radio.getaktiviert(receivedData, radio.e3aktiviert.mc)) {
-        if (radio.getByte(receivedData, radio.eBufferPointer.md, radio.eBufferOffset.b1_Servo) == 1) {
-            while (cb2.beispielSpurfolger(radio.getByte(receivedData, radio.eBufferPointer.mc, radio.eBufferOffset.b0_Motor), radio.getByte(receivedData, radio.eBufferPointer.md, radio.eBufferOffset.b0_Motor), radio.getAbstand(receivedData))) {
+        if (radio.getByte(receivedData, radio.eBufferPointer.md, radio.eBufferOffset.b1_Servo) == 2) {
+            if (cb2.beispielSpurfolger(radio.getByte(receivedData, radio.eBufferPointer.mc, radio.eBufferOffset.b0_Motor), radio.getByte(receivedData, radio.eBufferPointer.md, radio.eBufferOffset.b0_Motor), radio.getAbstand(receivedData))) {
             	
             }
         }
-    } else {
-    	
     }
     receiver.rgbLEDs(receiver.eRGBled.a, 0x0000ff, true)
     radio.zeige5x5Buffer(receivedData)
@@ -41,4 +42,5 @@ true,
 storage.getNumber(StorageSlots.s1)
 )
 storage.putNumber(StorageSlots.s1, cb2.storageBufferGet())
+cb2.writeReset()
 let _4digit = grove.createDisplay(DigitalPin.C16, DigitalPin.C17)
