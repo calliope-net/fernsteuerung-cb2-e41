ec57010f17fcf07ef23a2d7b5cc2bf0c831a51a3
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-07-14 15:18:18 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -25,26 +25,32 @@ btf.onReceivedData(function (receivedData) {
     } else if (btf.isBetriebsart(receivedData, btf.e0Betriebsart.p0) && btf.getaktiviert(receivedData, btf.e3aktiviert.mc)) {
         btf.comment(btf.btf_text("dauerhaft"))
     }
-    d1 = btf.isBetriebsart(btf.btf_receivedBuffer19(), btf.e0Betriebsart.p0) && (btf.getaktiviert(btf.btf_receivedBuffer19(), btf.e3aktiviert.mc) && btf.getByte(btf.btf_receivedBuffer19(), btf.eBufferPointer.md, btf.eBufferOffset.b1_Servo) == 2)
+    d2 = btf.isBetriebsart(btf.btf_receivedBuffer19(), btf.e0Betriebsart.p0) && (btf.getaktiviert(btf.btf_receivedBuffer19(), btf.e3aktiviert.mc) && btf.getByte(btf.btf_receivedBuffer19(), btf.eBufferPointer.md, btf.eBufferOffset.b1_Servo) == 2)
+    d3 = btf.isBetriebsart(btf.btf_receivedBuffer19(), btf.e0Betriebsart.p0) && (btf.getaktiviert(btf.btf_receivedBuffer19(), btf.e3aktiviert.mc) && btf.getByte(btf.btf_receivedBuffer19(), btf.eBufferPointer.md, btf.eBufferOffset.b1_Servo) == 3)
     receiver.rgbLEDs(receiver.eRGBled.a, 0x0000ff, true)
     btf.zeige5x5Buffer(receivedData)
     btf.zeige5x5Joystick(receivedData)
 })
+let dSchleife = false
+let d3 = false
 let d2 = false
-let d1 = false
 cb2.beimStart(true)
 basic.forever(function () {
-    if (d1 && !(btf.timeout(1000))) {
-        d2 = true
+    if (d2 && !(btf.timeout(1000))) {
+        dSchleife = true
         cb2.beispielSpurfolger(
         btf.getByte(btf.btf_receivedBuffer19(), btf.eBufferPointer.mc, btf.eBufferOffset.b0_Motor),
         btf.getByte(btf.btf_receivedBuffer19(), btf.eBufferPointer.md, btf.eBufferOffset.b0_Motor),
         btf.getSensor(btf.btf_receivedBuffer19(), btf.eBufferPointer.mc, btf.eSensor.b6),
         btf.getAbstand(btf.btf_receivedBuffer19())
         )
-    } else if (d2) {
-        d2 = false
+    } else if (d3 && !(btf.timeout(1000))) {
+        btf.comment(cb2.seite9Linienfolger(100, 50, 10))
+    } else if (dSchleife) {
+        dSchleife = false
         cb2.writeMotorenStop()
+    } else {
+    	
     }
 })
 loops.everyInterval(700, function () {
