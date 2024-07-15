2e5ff84bc20df202619464a4b6506034fd9fc66c
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-07-15 20:41:19 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,7 +8,7 @@ input.onButtonEvent(Button.B, sender.buttonEventValue(ButtonEvent.Hold), functio
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     btf.set_localProgram(true)
-    d3 = !(d3)
+    d1 = !(d1)
 })
 function fahrenJoystick () {
     if (btf.getSensor(btf.btf_receivedBuffer19(), btf.eBufferPointer.m0, btf.eSensor.b6) && (btf.getByte(btf.btf_receivedBuffer19(), btf.eBufferPointer.m0, btf.eBufferOffset.b0_Motor) > 128 && cb2.readUltraschallAbstand() < btf.getAbstand(btf.btf_receivedBuffer19()))) {
@@ -26,26 +26,26 @@ btf.onReceivedData(function (receivedData) {
     } else if (btf.isBetriebsart(receivedData, btf.e0Betriebsart.p0) && btf.getaktiviert(receivedData, btf.e3aktiviert.mc)) {
         btf.comment(btf.btf_text("dauerhaft"))
     }
+    d1 = btf.isBetriebsart(btf.btf_receivedBuffer19(), btf.e0Betriebsart.p1) && (btf.getaktiviert(btf.btf_receivedBuffer19(), btf.e3aktiviert.mc) && btf.getByte(btf.btf_receivedBuffer19(), btf.eBufferPointer.md, btf.eBufferOffset.b1_Servo) == 1)
     d2 = btf.isBetriebsart(btf.btf_receivedBuffer19(), btf.e0Betriebsart.p0) && (btf.getaktiviert(btf.btf_receivedBuffer19(), btf.e3aktiviert.mc) && btf.getByte(btf.btf_receivedBuffer19(), btf.eBufferPointer.md, btf.eBufferOffset.b1_Servo) == 2)
-    d3 = btf.isBetriebsart(btf.btf_receivedBuffer19(), btf.e0Betriebsart.p0) && (btf.getaktiviert(btf.btf_receivedBuffer19(), btf.e3aktiviert.mc) && btf.getByte(btf.btf_receivedBuffer19(), btf.eBufferPointer.md, btf.eBufferOffset.b1_Servo) == 3)
     receiver.rgbLEDs(receiver.eRGBled.a, 0x0000ff, true)
     btf.zeige5x5Buffer(receivedData)
     btf.zeige5x5Joystick(receivedData)
 })
 let bWiederholung = false
 let d2 = false
-let d3 = false
+let d1 = false
 cb2.beimStart(true)
 basic.forever(function () {
     if (d2 && !(btf.timeout(1000))) {
         bWiederholung = true
         cb2.beispielSpurfolger(btf.getByte(btf.btf_receivedBuffer19(), btf.eBufferPointer.mc, btf.eBufferOffset.b0_Motor), btf.getByte(btf.btf_receivedBuffer19(), btf.eBufferPointer.md, btf.eBufferOffset.b0_Motor), btf.getSensor(btf.btf_receivedBuffer19(), btf.eBufferPointer.mc, btf.eSensor.b6), btf.getAbstand(btf.btf_receivedBuffer19()))
-    } else if (d3 && !(btf.timeout(1000))) {
+    } else if (d1 && !(btf.timeout(1000))) {
         cb2.beispielSpurfolger16(
         192,
         31,
         bWiederholung,
-        20
+        btf.getAbstand(btf.btf_receivedBuffer19())
         )
         bWiederholung = true
     } else if (bWiederholung) {
