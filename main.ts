ed9e6352453b7dd45a2df721250b8cec47c3e8aa
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-07-16 14:18:45 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,20 +8,13 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     btf.set_localProgram(true)
     dauerhaft_1 = !(dauerhaft_1)
 })
-function fahrenJoystick () {
-    if (true && (btf.getByte(btf.btf_receivedBuffer19(), btf.eBufferPointer.m0, btf.eBufferOffset.b0_Motor) > 128 && cb2.readUltraschallAbstand() < btf.getAbstand(btf.btf_receivedBuffer19()))) {
-        cb2.writeMotorenStop()
-    } else {
-        cb2.writeMotor128Servo16(btf.getByte(btf.btf_receivedBuffer19(), btf.eBufferPointer.m0, btf.eBufferOffset.b0_Motor), btf.getByte(btf.btf_receivedBuffer19(), btf.eBufferPointer.m0, btf.eBufferOffset.b1_Servo), 45)
-    }
-}
 input.onButtonEvent(Button.A, sender.buttonEventValue(ButtonEvent.Hold), function () {
     btf.setFunkgruppeButton(btf.eFunkgruppeButton.minus)
 })
 btf.onReceivedData(function (receivedData) {
     dauerhaft_1 = btf.isBetriebsart(btf.btf_receivedBuffer19(), btf.e0Betriebsart.p1) && (btf.getaktiviert(btf.btf_receivedBuffer19(), btf.e3aktiviert.mc) && btf.getByte(btf.btf_receivedBuffer19(), btf.eBufferPointer.md, btf.eBufferOffset.b1_Servo) == 1)
     if (btf.isBetriebsart(receivedData, btf.e0Betriebsart.p0) && btf.getaktiviert(receivedData, btf.e3aktiviert.m0)) {
-        cb2.fahreJoystick(btf.btf_receivedBuffer19(), 50)
+        cb2.fahreJoystick(btf.btf_receivedBuffer19(), 40)
     } else if (btf.isBetriebsart(receivedData, btf.e0Betriebsart.p2)) {
         cb2.fahreBuffer19(btf.btf_receivedBuffer19())
     }
