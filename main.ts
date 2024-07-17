c246799c15ae6672f5f6729d110d0b2918ff4cf3
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-07-17 14:14:27 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,12 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
 	
 })
-input.onButtonEvent(Button.B, sender.buttonEventValue(ButtonEvent.Hold), function () {
-    btf.setFunkgruppeButton(btf.eFunkgruppeButton.plus)
-})
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     btf.set_localProgram(true)
     dauerhaft_Knopf_B = !(dauerhaft_Knopf_B)
 })
-input.onButtonEvent(Button.A, sender.buttonEventValue(ButtonEvent.Hold), function () {
-    btf.setFunkgruppeButton(btf.eFunkgruppeButton.minus)
+input.onButtonEvent(Button.B, btf.buttonEventValue(ButtonEvent.Hold), function () {
+    btf.setFunkgruppeButton(btf.eFunkgruppeButton.plus)
 })
 btf.onReceivedData(function (receivedData) {
     dauerhaft_Beispiel_1 = btf.isBetriebsart(btf.btf_receivedBuffer19(), btf.e0Betriebsart.p1) && (btf.getaktiviert(btf.btf_receivedBuffer19(), btf.e3aktiviert.mc) && btf.getByte(btf.btf_receivedBuffer19(), btf.eBufferPointer.md, btf.eBufferOffset.b1_Servo) == 1)
@@ -21,6 +18,9 @@ btf.onReceivedData(function (receivedData) {
     receiver.rgbLEDs(receiver.eRGBled.a, 0x0000ff, true)
     btf.zeige5x5Buffer(receivedData)
     btf.zeige5x5Joystick(receivedData)
+})
+input.onButtonEvent(Button.A, btf.buttonEventValue(ButtonEvent.Hold), function () {
+    btf.setFunkgruppeButton(btf.eFunkgruppeButton.minus)
 })
 let bWiederholung = false
 let dauerhaft_Beispiel_1 = false
