a6b7f662385103291546c8d50a25b683a769e2ff
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-08-08 20:34:28 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -30,13 +30,31 @@ btf.onReceivedDataChanged(function (receivedData, changed) {
 input.onButtonEvent(Button.A, btf.buttonEventValue(ButtonEvent.Hold), function () {
     btf.buttonAhold()
 })
+let bWiederholung = false
 let dauerhaft_Spurfolger = false
 let dauerhaft_Knopf_B = false
 cb2.beimStart()
 btf.zeigeBIN(cb2.readVersionArray()[1], btf.ePlot.bin, 2)
 btf.zeigeBIN(cb2.readSpannung(), btf.ePlot.bcd, 4)
 basic.forever(function () {
-    cb2.dauerhaft_SpurfolgerBuffer(btf.btf_receivedBuffer19(), dauerhaft_Spurfolger, cb2.eI2C.x22)
+    cb2.dauerhaft_SpurfolgerBuffer(dauerhaft_Spurfolger, btf.btf_receivedBuffer19(), cb2.eI2C.x21)
+    if (dauerhaft_Knopf_B && !(btf.timeout(60000, true))) {
+        cb2.beispielSpurfolger16(
+        192,
+        160,
+        31,
+        0,
+        bWiederholung,
+        true,
+        20,
+        cb2.eI2C.x22
+        )
+        bWiederholung = true
+    } else if (bWiederholung) {
+        dauerhaft_Knopf_B = false
+        bWiederholung = false
+        cb2.writeMotorenStop()
+    }
 })
 loops.everyInterval(700, function () {
     if (btf.timeout(1000)) {
