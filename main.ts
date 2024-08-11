eefb752f577e3ef002e09db14fb21f398d30b0d1
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-08-11 20:30:46 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,15 @@ input.onButtonEvent(Button.AB, btf.buttonEventValue(ButtonEvent.Hold), function 
 })
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     btf.set_timeoutDisbled(true)
+    abstand_Knopf_A = !(abstand_Knopf_A)
+    if (abstand_Knopf_A) {
+        fahreAbstand(192)
+    } else {
+        cb2.writeMotorenStop()
+    }
+})
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+    btf.set_timeoutDisbled(true)
     cb2.fahreStrecke(192, 31, 40)
     cb2.fahreStrecke(64, 31, 40)
     cb2.fahreStrecke(255, 16, 20)
@@ -13,10 +22,14 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     btf.set_timeoutDisbled(true)
     dauerhaft_Knopf_B = !(dauerhaft_Knopf_B)
 })
+function fahreAbstand (speed: number) {
+    cb2.writeMotor128Servo16(speed, 16)
+}
 input.onButtonEvent(Button.B, btf.buttonEventValue(ButtonEvent.Hold), function () {
     btf.buttonBhold()
 })
 btf.onReceivedDataChanged(function (receivedData, changed) {
+    abstand_Knopf_A = false
     dauerhaft_Knopf_B = false
     dauerhaft_Spurfolger = cb2.set_dauerhaft_Spurfolger(btf.btf_receivedBuffer19(), btf.e3aktiviert.mc)
     cb2.fahreJoystick(btf.btf_receivedBuffer19(), 50)
@@ -27,40 +40,63 @@ btf.onReceivedDataChanged(function (receivedData, changed) {
     btf.zeige5x5Joystick(receivedData)
     receiver.digitalWritePin(receiver.eDigitalPins.C16, !(btf.getSchalter(receivedData, btf.e0Schalter.b0)))
 })
-input.onButtonEvent(Button.A, btf.buttonEventValue(ButtonEvent.Hold), function () {
-    btf.buttonAhold()
+cb2.onStopEvent(function (abstand_Stop, cm) {
+    if (abstand_Knopf_A) {
+        btf.reset_timer()
+        if (abstand_Stop) {
+            if (Math.randomBoolean()) {
+                cb2.writeMotor128Servo16(64, randint(1, 9))
+            } else {
+                cb2.writeMotor128Servo16(64, randint(23, 31))
+            }
+        } else {
+            basic.pause(1000)
+            fahreAbstand(255)
+        }
+    }
 })
-let bWiederholung = false
-let dauerhaft_Spurfolger = false
-let dauerhaft_Knopf_B = false
-cb2.beimStart()
-btf.zeigeBIN(cb2.readVersionArray()[1], btf.ePlot.bin, 2)
-btf.zeigeBIN(cb2.readSpannung(), btf.ePlot.bcd, 4)
-basic.forever(function () {
-    cb2.dauerhaft_SpurfolgerBuffer(dauerhaft_Spurfolger, btf.btf_receivedBuffer19(), cb2.eI2C.x21)
+function dauerhaft_Knopf_B_Spurfolger () {
     if (dauerhaft_Knopf_B && !(btf.timeout(60000, true))) {
         cb2.beispielSpurfolger16(
         192,
         160,
         31,
         0,
-        bWiederholung,
+        dauerhaft_Wiederholung,
         true,
         20,
         cb2.eI2C.x22
         )
-        bWiederholung = true
-    } else if (bWiederholung) {
+        dauerhaft_Wiederholung = true
+    } else if (dauerhaft_Wiederholung) {
         dauerhaft_Knopf_B = false
-        bWiederholung = false
+        dauerhaft_Wiederholung = false
         cb2.writeMotorenStop()
     }
+}
+input.onButtonEvent(Button.A, btf.buttonEventValue(ButtonEvent.Hold), function () {
+    btf.buttonAhold()
+})
+let dauerhaft_Wiederholung = false
+let dauerhaft_Spurfolger = false
+let dauerhaft_Knopf_B = false
+let abstand_Knopf_A = false
+cb2.beimStart()
+btf.zeigeBIN(cb2.readVersionArray()[1], btf.ePlot.bin, 2)
+btf.zeigeBIN(cb2.readSpannung(), btf.ePlot.bcd, 4)
+abstand_Knopf_A = false
+basic.forever(function () {
+    cb2.dauerhaft_SpurfolgerBuffer(dauerhaft_Spurfolger, btf.btf_receivedBuffer19(), cb2.eI2C.x21)
+    if (abstand_Knopf_A) {
+        cb2.raiseAbstandEvent(40, randint(60, 120), 25)
+    }
+    dauerhaft_Knopf_B_Spurfolger()
 })
 loops.everyInterval(700, function () {
-    if (btf.timeout(1000)) {
+    if (btf.timeout(30000, true)) {
         cb2.writeMotorenStop()
-    }
-    if (btf.timeout(1000)) {
+        control.reset()
+    } else if (btf.timeout(1000)) {
         receiver.setLedColors(receiver.eRGBled.a, 0xff0000, true, true)
     } else if (btf.timeout(1000, true)) {
         receiver.setLedColors(receiver.eRGBled.a, 0x00ff00, true)
