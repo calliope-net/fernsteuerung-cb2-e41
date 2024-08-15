0826fcbbcfca17fc143350a1bc9fa114ef7e58ad
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-08-15 19:56:32 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -36,7 +36,7 @@ input.onButtonEvent(Button.B, btf.buttonEventValue(ButtonEvent.Hold), function (
 btf.onReceivedDataChanged(function (receivedData, changed) {
     abstand_Knopf_A = false
     spur_Knopf_B = false
-    dauerhaft_Spurfolger = cb2.set_dauerhaft_Spurfolger(btf.btf_receivedBuffer19(), btf.e3aktiviert.mc)
+    Spur_folgen = cb2.set_Spur_folgen(btf.btf_receivedBuffer19(), btf.e3aktiviert.mc)
     dauerhaft_Ausweichen = cb2.set_AbstandAusweichen(btf.btf_receivedBuffer19(), btf.e3aktiviert.md)
     cb2.fahreJoystick(btf.btf_receivedBuffer19(), 50)
     cb2.fahrplanBuffer5Strecken(btf.btf_receivedBuffer19(), btf.e3aktiviert.m1)
@@ -45,6 +45,21 @@ btf.onReceivedDataChanged(function (receivedData, changed) {
     btf.zeige5x5Buffer(receivedData)
     btf.zeige5x5Joystick(receivedData)
     pins.pinDigitalWrite(pins.pins_eDigitalPins(pins.eDigitalPins.C16), !(btf.getSchalter(receivedData, btf.e0Schalter.b0)))
+})
+cb2.onSensorEvent(function (links_hell, rechts_hell, abstand_Stop, cm) {
+    cb2.buffer_Spur_folgen(Spur_folgen, links_hell, rechts_hell, abstand_Stop, btf.btf_receivedBuffer19())
+    cb2.eventSpurfolger(
+    spur_Knopf_B,
+    links_hell,
+    rechts_hell,
+    abstand_Stop,
+    192,
+    160,
+    31,
+    0,
+    abstand_Knopf_A,
+    30
+    )
 })
 function dauerhaft_Knopf_B_Spurfolger () {
     if (spur_Knopf_B) {
@@ -69,7 +84,7 @@ input.onButtonEvent(Button.A, btf.buttonEventValue(ButtonEvent.Hold), function (
     btf.buttonAhold()
 })
 let spur_gestartet = false
-let dauerhaft_Spurfolger = false
+let Spur_folgen = false
 let spur_Knopf_B = false
 let dauerhaft_Ausweichen = false
 let abstand_Knopf_A = false
@@ -78,8 +93,8 @@ btf.zeigeBIN(cb2.readVersionArray()[1], btf.ePlot.bin, 2)
 btf.zeigeBIN(cb2.readSpannung(), btf.ePlot.bcd, 4)
 abstand_Knopf_A = false
 basic.forever(function () {
-    cb2.dauerhaft_SpurfolgerBuffer(dauerhaft_Spurfolger, btf.btf_receivedBuffer19(), cb2.eI2C.x21)
     cb2.raiseAbstandEvent(abstand_Knopf_A || dauerhaft_Ausweichen, 40, 45)
+    cb2.raiseSpurEvent(spur_Knopf_B)
     dauerhaft_Knopf_B_Spurfolger()
 })
 loops.everyInterval(700, function () {
