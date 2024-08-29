11a259260ecafbbfdaf491c890944cd7060f242a
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-08-29 20:42:03 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,28 +3,31 @@ input.onButtonEvent(Button.AB, btf.buttonEventValue(ButtonEvent.Hold), function 
 })
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     if (receiver.isFunktion(receiver.eFunktion.ng)) {
+        Stop = 40
         receiver.setFunktion(receiver.eFunktion.hindernis_ausweichen)
-        btf.set_timeoutDisbled(true)
     } else if (receiver.isFunktion(receiver.eFunktion.hindernis_ausweichen)) {
         receiver.setFunktion(receiver.eFunktion.ng)
-        btf.set_timeoutDisbled(false)
     } else if (receiver.isFunktion(receiver.eFunktion.spur_folgen)) {
+        Stop = 20
         Ultraschall_Sensor_Knopf_A = !(Ultraschall_Sensor_Knopf_A)
     }
 })
 input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
-    btf.set_timeoutDisbled(true)
-    cb2.fahreStrecke(192, 31, 40)
-    cb2.fahreStrecke(64, 31, 40)
-    cb2.fahreStrecke(255, 16, 20)
-    cb2.fahreStrecke(192, 2, 150)
-    cb2.fahreStrecke(192, 30, 150)
-    cb2.fahreStrecke(1, 16, 20)
+    if (receiver.isFunktion(receiver.eFunktion.ng)) {
+        receiver.setFunktion(receiver.eFunktion.fahrplan)
+        cb2.fahreStrecke(192, 31, 40)
+        cb2.fahreStrecke(64, 31, 40)
+        cb2.fahreStrecke(255, 16, 20)
+        cb2.fahreStrecke(192, 2, 150)
+        cb2.fahreStrecke(192, 30, 150)
+        cb2.fahreStrecke(1, 16, 20)
+        receiver.setFunktion(receiver.eFunktion.ng)
+    }
 })
 cb2.onAbstandEvent(function (abstand_Sensor, abstand_Stop, cm) {
     cb2.buffer_Hindernis_ausweichen(btf.btf_receivedBuffer19(), abstand_Stop)
     cb2.event_Hindernis_ausweichen(
-    Ultraschall_Sensor_Knopf_A && !(Spur_Sensor_Knopf_B),
+    receiver.isFunktion(receiver.eFunktion.hindernis_ausweichen),
     abstand_Stop,
     255,
     16,
@@ -41,10 +44,8 @@ cb2.onAbstandEvent(function (abstand_Sensor, abstand_Stop, cm) {
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     if (receiver.isFunktion(receiver.eFunktion.ng)) {
         receiver.setFunktion(receiver.eFunktion.spur_folgen)
-        btf.set_timeoutDisbled(true)
     } else if (receiver.isFunktion(receiver.eFunktion.spur_folgen)) {
         receiver.setFunktion(receiver.eFunktion.ng)
-        btf.set_timeoutDisbled(false)
     }
 })
 input.onButtonEvent(Button.B, btf.buttonEventValue(ButtonEvent.Hold), function () {
@@ -55,8 +56,8 @@ btf.onReceivedDataChanged(function (receivedData, changed) {
         cb2.writeMotorenStop()
         cb2.writecb2RgbLeds(0x000000, false)
     }
+    receiver.setFunktion(receiver.eFunktion.ng)
     Ultraschall_Sensor_Knopf_A = false
-    Spur_Sensor_Knopf_B = false
     cb2.fahreJoystick(btf.btf_receivedBuffer19(), 50)
     cb2.fahrplanBuffer5Strecken(btf.btf_receivedBuffer19(), btf.e3aktiviert.m1)
     cb2.fahrplanBuffer2x2Motoren(btf.btf_receivedBuffer19(), btf.e3aktiviert.ma)
@@ -73,7 +74,7 @@ function Konfiguration () {
 cb2.onSpurEvent(function (links_hell, rechts_hell, abstand_Stop) {
     cb2.buffer_Spur_folgen(btf.btf_receivedBuffer19(), links_hell, rechts_hell, abstand_Stop)
     cb2.event_Spur_folgen(
-    Spur_Sensor_Knopf_B,
+    receiver.isFunktion(receiver.eFunktion.spur_folgen),
     links_hell,
     rechts_hell,
     192,
@@ -87,17 +88,16 @@ cb2.onSpurEvent(function (links_hell, rechts_hell, abstand_Stop) {
 input.onButtonEvent(Button.A, btf.buttonEventValue(ButtonEvent.Hold), function () {
     btf.buttonAhold()
 })
-let Spur_Sensor_Knopf_B = false
 let Ultraschall_Sensor_Knopf_A = false
+let Stop = 0
 cb2.beimStart()
 btf.zeigeBIN(cb2.readVersionArray()[1], btf.ePlot.bin, 2)
 btf.zeigeBIN(cb2.readSpannung(), btf.ePlot.bcd, 4)
-Ultraschall_Sensor_Knopf_A = false
 basic.forever(function () {
     cb2.buffer_raiseAbstandEvent(btf.btf_receivedBuffer19())
     cb2.buffer_raiseSpurEvent(btf.btf_receivedBuffer19())
-    cb2.raiseAbstandEvent(Ultraschall_Sensor_Knopf_A, 30, 35)
-    cb2.raiseSpurEvent(Spur_Sensor_Knopf_B)
+    cb2.raiseAbstandEvent(receiver.isFunktion(receiver.eFunktion.hindernis_ausweichen) || Ultraschall_Sensor_Knopf_A, Stop, Stop + 5)
+    cb2.raiseSpurEvent(receiver.isFunktion(receiver.eFunktion.spur_folgen))
 })
 loops.everyInterval(700, function () {
     if (btf.timeout(30000, true)) {
